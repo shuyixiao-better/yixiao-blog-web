c88cad0d0c404ff8f4bbb05b44a08a04d07f66f4
Shujh <chinasjh2022@126.com>
2024-06-03 17:26:37 +0800

    :tada: 首次尝试访问后端服务修改

--- src/main.js
+++ src/main.js
@@ -4,9 +4,11 @@ import { createPinia } from 'pinia';
 import { createApp } from 'vue';
 import App from './App.vue';
 import httpInstance from './utils/http';  // 引入 axios 实例
+import Antd from 'ant-design-vue';
 
 // 测试接口函数
 import { getCategoryAPI } from '@/apis/test.js';
+import router from "@/router/index.js";
 getCategoryAPI().then(res => {
     console.log(res);
 });
@@ -16,6 +18,8 @@ const app = createApp(App);
 
 const pinia = createPinia();
 app.use(pinia);
+app.use(router);
+app.use(Antd);
 app.mount('#app');
 
 // 将 httpInstance 挂载到全局属性中，便于在组件中使用
