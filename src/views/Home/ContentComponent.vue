70747dee49b9124e71996483e2cce26e1cb467b3
Shujh <chinasjh2022@126.com>
2024-06-06 17:49:04 +0800

    :sparkles: 实现导航栏铺满整个页面

--- src/views/Home/ContentComponent.vue
+++ src/views/Home/ContentComponent.vue
@@ -1,78 +1,81 @@
 <template>
-  <el-tabs v-model="activeName" class="demo-tabs" @tab-click="handleClick">
-    <el-tab-pane name="first">
-      <template #label>
-        <div class="custom-tabs-label">
-          <el-icon :size="26"><HomeIcon /></el-icon>
-          <span>首页</span>
-        </div>
-      </template>
-      <a href="https://blog.csdn.net/weixin_50503886?spm=1000.2115.3001.5343" target="_blank">关于我</a>
-    </el-tab-pane>
-    <el-tab-pane label="文章归档" name="second">
-      <template #label>
-        <div class="custom-tabs-label">
-          <el-icon :size="26"><ArticleArchiveIcon /></el-icon>
-          <span>文章归档</span>
-        </div>
-      </template>
-      Config
-    </el-tab-pane>
-    <el-tab-pane label="说说" name="third">
-      <template #label>
-        <div class="custom-tabs-label">
-          <el-icon :size="26"><TalkAboutIcon /></el-icon>
-          <span>说说</span>
-        </div>
-      </template>
-      Role
-    </el-tab-pane>
-    <el-tab-pane label="网址导航" name="fourth">
-      <template #label>
-        <div class="custom-tabs-label">
-          <el-icon :size="26"><UrlNavigationIcon /></el-icon>
-          <span>网址导航</span>
-        </div>
-      </template>
-      Task
-    </el-tab-pane>
-    <el-tab-pane label="热搜" name="five">
-      <template #label>
-        <div class="custom-tabs-label">
-          <el-icon :size="26"><HotSearchIcon /></el-icon>
-          <span>热搜</span>
-        </div>
-      </template>
-      Task
-    </el-tab-pane>
-    <el-tab-pane label="留言版" name="six">
-      <template #label>
-        <div class="custom-tabs-label">
-          <el-icon :size="26"><NoticeBoardIcon /></el-icon>
-          <span>留言版</span>
-        </div>
-      </template>
-      Task
-    </el-tab-pane>
-    <el-tab-pane label="友情连接" name="seven">
-      <template #label>
-        <div class="custom-tabs-label">
-          <el-icon :size="26"><LinkIcon /></el-icon>
-          <span>友情连接</span>
-        </div>
-      </template>
-      <a href="https://blog.csdn.net/weixin_50503886?spm=1000.2115.3001.5343" target="_blank">博客链接</a>
-    </el-tab-pane>
-    <el-tab-pane label="关于本站" name="eight">
-      <template #label>
-        <div class="custom-tabs-label">
-          <el-icon :size="26"><AboutTheSiteIcon /></el-icon>
-          <span>关于本站</span>
-        </div>
-      </template>
-      Task
-    </el-tab-pane>
-  </el-tabs>
+  <div class="headStyle">
+    <el-tabs v-model="activeName" class="navigationBar" @tab-click="handleClick">
+
+      <el-tab-pane name="first">
+        <template #label>
+          <div class="custom-tabs-label">
+            <el-icon :size="26"><HomeIcon /></el-icon>
+            <span>首页</span>
+          </div>
+        </template>
+        <a href="https://blog.csdn.net/weixin_50503886?spm=1000.2115.3001.5343" target="_blank">关于我</a>
+      </el-tab-pane>
+      <el-tab-pane label="文章归档" name="second">
+        <template #label>
+          <div class="custom-tabs-label">
+            <el-icon :size="26"><ArticleArchiveIcon /></el-icon>
+            <span>文章归档</span>
+          </div>
+        </template>
+        Config
+      </el-tab-pane>
+      <el-tab-pane label="说说" name="third">
+        <template #label>
+          <div class="custom-tabs-label">
+            <el-icon :size="26"><TalkAboutIcon /></el-icon>
+            <span>说说</span>
+          </div>
+        </template>
+        Role
+      </el-tab-pane>
+      <el-tab-pane label="网址导航" name="fourth">
+        <template #label>
+          <div class="custom-tabs-label">
+            <el-icon :size="26"><UrlNavigationIcon /></el-icon>
+            <span>网址导航</span>
+          </div>
+        </template>
+        Task
+      </el-tab-pane>
+      <el-tab-pane label="热搜" name="five">
+        <template #label>
+          <div class="custom-tabs-label">
+            <el-icon :size="26"><HotSearchIcon /></el-icon>
+            <span>热搜</span>
+          </div>
+        </template>
+        Task
+      </el-tab-pane>
+      <el-tab-pane label="留言版" name="six">
+        <template #label>
+          <div class="custom-tabs-label">
+            <el-icon :size="26"><NoticeBoardIcon /></el-icon>
+            <span>留言版</span>
+          </div>
+        </template>
+        Task
+      </el-tab-pane>
+      <el-tab-pane label="友情连接" name="seven">
+        <template #label>
+          <div class="custom-tabs-label">
+            <el-icon :size="26"><LinkIcon /></el-icon>
+            <span>友情连接</span>
+          </div>
+        </template>
+        <a href="https://blog.csdn.net/weixin_50503886?spm=1000.2115.3001.5343" target="_blank">博客链接</a>
+      </el-tab-pane>
+      <el-tab-pane label="关于本站" name="eight">
+        <template #label>
+          <div class="custom-tabs-label">
+            <el-icon :size="26"><AboutTheSiteIcon /></el-icon>
+            <span>关于本站</span>
+          </div>
+        </template>
+        Task
+      </el-tab-pane>
+    </el-tabs>
+  </div>
 </template>
 <script lang="ts" setup>
 import { ref } from 'vue'
@@ -93,11 +96,19 @@ const handleClick = (tab: TabsPaneContext, event: Event) => {
 }
 </script>
 
-<style>
-.demo-tabs {
-  padding: 32px;
+<style scoped lang="scss">
+.navigationBar {
+  margin: 0 auto;
   color: #6b778c;
   font-size: 32px;
   font-weight: 600;
+  width: 100%;
 }
+.headStyle{
+  margin-bottom: 20px;
+  box-shadow: 0 2px 4px rgba(0, 0, 0, 0.15);
+  width: 100%;
+  padding: 45px 0 12px 0;
+}
+
 </style>
