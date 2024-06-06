42b181e6ae8247e284f3bacc5a4c33685d616adb
Shujh <chinasjh2022@126.com>
2024-06-06 14:44:12 +0800

    :fire: 移除部分文字描述便于开发

--- src/views/Home/ContentComponent.vue
+++ src/views/Home/ContentComponent.vue
@@ -1,6 +1,8 @@
 <template>
   <el-tabs v-model="activeName" class="demo-tabs" @tab-click="handleClick">
-    <el-tab-pane label="首页" name="first">关于我</el-tab-pane>
+    <el-tab-pane label="首页" name="first">
+      <a href="https://blog.csdn.net/weixin_50503886?spm=1000.2115.3001.5343" target="_blank">关于我</a>
+    </el-tab-pane>
     <el-tab-pane label="文章归档" name="second">Config</el-tab-pane>
     <el-tab-pane label="说说" name="third">Role</el-tab-pane>
     <el-tab-pane label="网址导航" name="fourth">Task</el-tab-pane>
@@ -11,7 +13,10 @@
     </el-tab-pane>
     <el-tab-pane label="关于本站" name="eight">Task</el-tab-pane>
   </el-tabs>
-
+  <div class="search-container">
+    <input type="text" class="search-input" placeholder="Search...">
+    <button class="search-button">🔍</button>
+  </div>
 </template>
 <script lang="ts" setup>
 import { ref } from 'vue'
@@ -25,10 +30,39 @@ const handleClick = (tab: TabsPaneContext, event: Event) => {
 </script>
 
 <style>
-.demo-tabs > .el-tabs__content {
+.demo-tabs {
   padding: 32px;
   color: #6b778c;
   font-size: 32px;
   font-weight: 600;
+}
+
+.search-container {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  padding: 32px;
+}
+.search-input {
+  width: 200px;
+  padding: 32px;
+  border: 1px solid #ccc;
+  border-radius: 4px 0 0 4px;
+  outline: none;
+  float: right;
+  transition: border-color 0.3s;
+}
+.search-button {
+  padding: 8px 12px;
+  border: 1px solid #007BFF;
+  border-left: none;
+  border-radius: 0 4px 4px 0;
+  background-color: #007BFF;
+  color: white;
+  cursor: pointer;
+  transition: background-color 0.3s;
+}
+.search-button:hover {
+  background-color: #0056b3;
 }
 </style>
