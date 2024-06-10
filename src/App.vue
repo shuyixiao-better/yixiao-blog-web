a7aa110cdf2df259907e8fe5e5fc9c41c01c86c9
ShuYixiao <1413230151@qq.com>
2024-06-10 22:25:51 +0800

    :sparkles: 实现首页花瓣飘落效果

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,12 @@
 <template>
+  <Petal
+      v-for="(petal, index) in petals"
+      :key="index"
+      :size="petal.size"
+      :left="petal.left"
+      :delay="petal.delay"
+      :duration="petal.duration"
+  />
   <header>
     <ContentComponent />
     <div class="content">
@@ -98,16 +106,44 @@
 
 <script>
 import ContentComponent from '@/views/Home/ContentComponent.vue';
+import Petal from '@/style/Petal.vue'
 
 export default {
   name: 'App',
   components: {
-    ContentComponent
+    ContentComponent,
+    Petal
+  },
+  data() {
+    return {
+      petals: []
+    };
+  },
+  created() {
+    this.generatePetals(20);
+  },
+  methods: {
+    generatePetals(count) {
+      for (let i = 0; i < count; i++) {
+        this.petals.push({
+          size: Math.random() * 20 + 20,
+          left: Math.random() * window.innerWidth,
+          delay: Math.random() * 10,
+          duration: Math.random() * 5 + 5
+        });
+      }
+    }
   }
 }
 </script>
 
 <style scoped lang="scss">
+#app {
+  position: relative;
+  overflow: hidden;
+  height: 100vh;
+  background: linear-gradient(to bottom, #87ceeb, #f0e68c);
+}
 .content {
   margin-left: 20%;
 }
---
+++ src/style/Petal.vue
@@ -0,0 +1,53 @@
+<template>
+  <div class="petal" :style="style"></div>
+</template>
+
+<script>
+export default {
+  name: 'Petal',
+  props: {
+    size: {
+      type: Number,
+      default: 30
+    },
+    left: {
+      type: Number,
+      default: 0
+    },
+    delay: {
+      type: Number,
+      default: 0
+    },
+    duration: {
+      type: Number,
+      default: 10
+    }
+  },
+  computed: {
+    style() {
+      return {
+        width: `${this.size}px`,
+        height: `${this.size}px`,
+        left: `${this.left}px`,
+        animationDelay: `${this.delay}s`,
+        animationDuration: `${this.duration}s`
+      };
+    }
+  }
+};
+</script>
+
+<style scoped>
+.petal {
+  position: absolute;
+  top: -100px;
+  background: url('/img/peach-flower.png') no-repeat center/contain;
+  animation: fall linear infinite;
+}
+
+@keyframes fall {
+  to {
+    transform: translateY(100vh) rotate(360deg);
+  }
+}
+</style>
