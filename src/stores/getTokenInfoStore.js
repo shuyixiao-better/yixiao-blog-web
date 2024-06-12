1d2e52117e41d46cb034cdc2a334a2f9f5e93af7
ShuYixiao <1413230151@qq.com>
2024-06-13 00:31:12 +0800

    :sparkles: 增加token信息

--- src/stores/getTokenInfoStore.js
+++ src/stores/getTokenInfoStore.js
@@ -6,11 +6,12 @@ import { getVerificationCode } from '@/apis/token/getVerificationCode.js'
 
 export const getTokenInfoStore = defineStore('user', () => {
     // 1. 定义获取token权限的state
-    const tokenInfo = ref({})
+    const serveTokenInfo = ref({})
     // 2. 定义获取接口数据的action函数
     const getTokenInfo = async () => {
         const res = await getVerificationCode()
-        tokenInfo.value = res.data
+        console.log("Response data:", res.data);
+        serveTokenInfo.value = res.data
     }
     // 3. 以对象的格式把state和action return
     return {
--- src/utils/http.js
+++ src/utils/http.js
@@ -1,5 +1,6 @@
 // src/utils/http.js
 import axios from 'axios';
+import { getTokenInfoStore } from "@/stores/getTokenInfoStore.js";
 
 // 创建 axios 实例
 const httpInstance = axios.create({
@@ -8,8 +9,15 @@ const httpInstance = axios.create({
 });
 
 // axios 请求拦截器
-httpInstance.interceptors.request.use(
-    config => {
+httpInstance.interceptors.request.use(config => {
+        // 1. 从pinia获取token数据
+        const TokenInfoStore = getTokenInfoStore()
+        // 2.按照后端要求拼接token数据
+        console.log("1111:", TokenInfoStore.serveTokenInfo)
+        const token = TokenInfoStore.serveTokenInfo
+        if (token){
+           config.headers.Authorization = `Bearer ${token}`
+        }
         return config;
     },
     error => Promise.reject(error)
